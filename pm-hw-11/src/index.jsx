187c90ffed902c15b104d5d252dc70df68bead6e
Sasha Hulitskyi <alexgulitskyy3@gmail.com>
2021-02-22 13:43:41 +0200

    added hw 11

---
+++ pm-hw-11/src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import WeaklyWeatherBlock from './WeaklyWeatherBlock';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <WeaklyWeatherBlock/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
