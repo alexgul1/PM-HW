f7be1f5bf40fa5a80fac3bf2bdec33bab4d3a681
Sasha Hulitskyi <alexgulitskyy3@gmail.com>
2021-03-06 21:25:05 +0200

    added routes for home and 404 pages

--- pm-hw-13/src/App.jsx
+++ pm-hw-13/src/App.jsx
@@ -14,8 +14,10 @@ function App() {
       <Menu/>
 
       <div className={styles.container}>
-        {/*TODO Add No Match and home pages*/}
         <Switch>
+          <Route exact path="/">
+            <h2>Home Page</h2>
+          </Route>
           <Route path="/weather">
             <WeeklyForecast />
           </Route>
@@ -25,7 +27,9 @@ function App() {
           <Route path="/todos">
             <Todos />
           </Route>
-
+          <Route path="*">
+            <h2>Page not found</h2>
+          </Route>
         </Switch>
       </div>
     </BrowserRouter>
